b7710b5a5858366ee6e57de3ee70367b714cb9e8
IMabhinavpratapsingh <abhinavpratapsingh890cr@gmail.com>
2025-10-16 09:26:52 +0530

    Add files via upload

---
+++ assignment_02/task_01.py
@@ -0,0 +1,13 @@
+num = int(input("enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
+
+# output >>
+
+# enter a number: 7
+# 7 is an odd number.
+
+# enter a number: 6
+# 6 is an even number.
